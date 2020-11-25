f9560a82e2510c534ee35c114a7d87699bb82c9e
antelle <dmitryvit@gmail.com>
2020-11-25 20:38:42 +0100

    testing the key on start

--- scripts/start.js
+++ scripts/start.js
@@ -1,4 +1,8 @@
-(async function() {
+(async function () {
+    await require('./sign')(Buffer.from('test'));
     await require('./download-translations')();
     require('./update-plugins');
-})();
+})().catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error('Error', err);
+});
